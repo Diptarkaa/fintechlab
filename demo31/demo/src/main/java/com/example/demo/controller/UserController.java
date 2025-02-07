c6db30666d0bfb764bdb1cfde0c34e0c3483a1d2
Diptarkaa <141421491+Diptarkaa@users.noreply.github.com>
2025-02-07 09:39:31 +0530

    Add files via upload

---
+++ demo31/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+import java.util.List;
+
+@RestController
+
+@RequestMapping("/api/users")
+
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+}
